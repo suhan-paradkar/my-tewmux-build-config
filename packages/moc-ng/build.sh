80b0b03478ad7031549bc6e9388a21aaf8fb5123
Suhan G Paradkar <12suhangp34@gmail.com>
2021-06-16 10:55:30 +0000

    update alsa-libs

---
+++ packages/moc-ng/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_DESCRIPTION="Moc recompiled with plugins for cmake"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_VERSION=3.1
+TERMUX_PKG_SRCURL=https://github.com/woboq/moc-ng.git
+TERMUX_PKG_GIT_BRANCH=master
