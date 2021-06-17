100511dcc2ab0610e28be82632b0b54054bd55b5
Suhan G Paradkar <12suhangp34@gmail.com>
2021-06-17 10:27:48 +0000

    update alsa

--- packages/alsa-lib/build.sh
+++ packages/alsa-lib/build.sh
@@ -1,12 +1,10 @@
 TERMUX_PKG_HOMEPAGE=http://www.alsa-project.org
 TERMUX_PKG_VERSION=1.2.5.1
-# TERMUX_PKG_DEPENDS="libandroid-shmem, python2"
-TERMUX_PKG_DEPENDS="libandroid-shmem"
+TERMUX_PKG_DEPENDS="libandroid-shmem, python"
 TERMUX_PKG_SRCURL=ftp://ftp.alsa-project.org/pub/lib/alsa-lib-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=628421d950cecaf234de3f899d520c0a6923313c964ad751ffac081df331438e
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--disable-python --disable-mixer"
 TERMUX_PKG_LICENSE="GPL-2.0"
-
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="--disable-mixer --enable-topology"
 termux_step_pre_configure() {
     CPPFLAGS+="-DTERMUX_SHMEM_STUBS -DTERMUX_SEMOPS_STUBS"
 }
--- packages/alsa-utils/build.sh
+++ packages/alsa-utils/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=http://www.alsa-project.org
 TERMUX_PKG_VERSION=1.2.5.1
 TERMUX_PKG_SRCURL=ftp://ftp.alsa-project.org/pub/utils/alsa-utils-$TERMUX_PKG_VERSION.tar.bz2
 TERMUX_PKG_SHA256=9c169ae37a49295f9b97b92ace772803daf6b6510a19574e0b78f87e562118d0
-TERMUX_PKG_DEPENDS="alsa-lib, ncurses"
-TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-udev-rules-dir=$TERMUX_PREFIX/lib/udev/rules.d --with-asound-state-dir=$TERMUX_PREFIX/var/lib/alsa --disable-bat --disable-rst2man"
-
+TERMUX_PKG_DEPENDS="alsa-lib, ncurses, ndk-sysroot"
+TERMUX_PKG_EXTRA_CONFIGURE_ARGS="--with-udev-rules-dir=$TERMUX_PREFIX/lib/udev/rules.d --with-asound-state-dir=$TERMUX_PREFIX/var/lib/alsa --disable-alsamixer --disable-rst2man --disable-alsatest --disable-bat"
+TERMUX_PKG_LICENSE="GPL-2.0"
 termux_step_pre_configure() {
-    LDFLAGS+=" -llog"
-    LDFLAGS+=" -llibasound"
+    LDFLAGS+=" -L@TERMUX_PREFIX@/lib"
+    LDFLAGS+=" -llog -lm -latopology"
 }
