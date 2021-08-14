e0a2add15b827f6ebd378ab68385d5e59c11efd9
Wisest_wizard <12suhangp34@gmail.com>
2021-08-14 12:42:56 +0530

    fix path

--- packages/musl-libc/build.sh
+++ packages/musl-libc/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_SRCURL=https://musl.libc.org/releases/musl-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9b969322012d796dc23dda27a35866034fa67d8fb67e0e2c45c913c3d43219dd
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64, i686"
 termux_step_configure() {
-	mkdir $TERMUX_PREFIX/opt/musl
-	./configure --prefix=$TERMUX_PREFIX/opt/musl
+	mkdir $TERMUX_PREFIX/musl
+	./configure --prefix=$TERMUX_PREFIX/musl
 }
 
