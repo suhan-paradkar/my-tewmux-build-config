32cd4e460b5f9fefbee3f3e03877819234bf50c2
Wisest_wizard <12suhangp34@gmail.com>
2021-08-14 12:31:47 +0530

    blacklist amd procesors

--- packages/musl-libc/build.sh
+++ packages/musl-libc/build.sh
@@ -5,4 +5,9 @@ TERMUX_PKG_VERSION=1.2.2
 TERMUX_PKG_MAINTAINER="@suhan-paradkar"
 TERMUX_PKG_SRCURL=https://musl.libc.org/releases/musl-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=9b969322012d796dc23dda27a35866034fa67d8fb67e0e2c45c913c3d43219dd
+TERMUX_PKG_BLACKLISTED_ARCH="x86_64, i686"
+termux_step_configure() {
+	nkdir $TERMUX_PREFIX/opt/musl
+	./configure --prefix=$TERMUX_PREFIX/opt/musl
+}
 
