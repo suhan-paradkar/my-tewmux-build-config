88c7760e8e6d1a2f5e891b33ff0dfaaf0c9b8a14
Wisest_wizard <12suhangp34@gmail.com>
2021-08-14 12:52:17 +0530

    fix libinput buildscript

--- packages/libinput/build.sh
+++ packages/libinput/build.sh
@@ -1,9 +1,10 @@
+TERMUX_PKG_HOMEPAGE=https://www.freedesktop.org/wiki/Software/libinput/
 TERMUX_PKG_DESCRIPTION="Input device management and event handling library"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://github.com/wayland-project/libinput.git
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="libudev-stub, mtdev, libevdev, gtk3"
-
+TERMUX_PKG_MAINTAINER="@suhan-paradkar"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dlibwacom=false \
 				-Ddocumentation=false"
