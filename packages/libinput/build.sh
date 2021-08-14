11df8cd93cc5b7c18fe7c1d9adbe287373371ad6
Wisest_wizard <12suhangp34@gmail.com>
2021-08-14 12:49:41 +0530

    kickstart libinput build

--- packages/libinput/build.sh
+++ packages/libinput/build.sh
@@ -4,5 +4,6 @@ TERMUX_PKG_VERSION=1.18.0
 TERMUX_PKG_SRCURL=https://github.com/wayland-project/libinput.git
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
 TERMUX_PKG_DEPENDS="libudev-stub, mtdev, libevdev, gtk3"
+
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dlibwacom=false \
 				-Ddocumentation=false"
