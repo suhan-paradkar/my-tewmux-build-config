dc398670461f7d4a4d96eb4c1a9beb6f1b6abf2b
Wisest_wizard <12suhangp34@gmail.com>
2021-08-14 12:09:52 +0530

    add package:musl libc

---
+++ packages/musl-libc/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_DESCRIPTION="musl is an implementation of the C standard library built on top of the Linux system call API"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_VERSION=1.2.2
+TERMUX_PKG_MAINTAINER="@suhan-paradkar"'
+TERMUX_PKG_SRCURL=https://musl.libc.org/releases/musl-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=GG
+
