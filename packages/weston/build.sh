215c47464f90c9fa05b269d9cd2810cb8583e1f6
wisest_wizard <12suhangp34@gmail.com>
2021-08-18 08:52:39 +0000

    Fix weston

--- packages/weston/build.sh
+++ packages/weston/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_VERSION=9.0.0
 TERMUX_PKG_SRCURL=https://github.com/wayland-project/weston.git
 TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
-TERMUX_PKG_DEPENDS="libinput, libxkbcommon, libwayland, libwayland-protocols"
+TERMUX_PKG_DEPENDS="libinput, libxkbcommon, libwayland, libwayland-protocols, libandroid-shmem"
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dimage-webp=false \
 			-Drenderer-gl=false \
 			-Dlauncher-logind=false \
@@ -15,3 +15,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS+="-Dimage-webp=false \
 			-Dremoting=false \
 			-Dpipewire=false \
 			-Ddemo-clients=false"
+termux_step_pre_configure() {
+        LDFLAGS+=" -landroid-shmem"
+}
