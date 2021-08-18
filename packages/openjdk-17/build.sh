93449d96882dbbe1b2cad07f0944f3172c4f0c31
wisest_wizard <12suhangp34@gmail.com>
2021-08-18 02:06:21 +0000

    Add few more changes

--- packages/openjdk-17/build.sh
+++ packages/openjdk-17/build.sh
@@ -6,7 +6,7 @@ TERMUX_PKG_VERSION=17.0
 TERMUX_PKG_REVISION=8
 TERMUX_PKG_SRCURL=https://github.com/termux/openjdk-mobile-termux/archive/ec285598849a27f681ea6269342cf03cf382eb56.tar.gz
 TERMUX_PKG_SHA256=d7c6ead9d80d0f60d98d0414e9dc87f5e18a304e420f5cd21f1aa3210c1a1528
-TERMUX_PKG_DEPENDS="freetype, libandroid-shmem, libandroid-spawn, libiconv, zlib, xorgproto, libx11, libxcursor, cups, fontconfig, libpng, libxrender, libxtst, libxrandr, libxt"
+TERMUX_PKG_DEPENDS="freetype, libandroid-shmem, libandroid-spawn, libiconv, zlib"
 TERMUX_PKG_BUILD_DEPENDS="cups, fontconfig, libpng, libx11, libxrender"
 TERMUX_PKG_SUGGESTS="cups, fontconfig, libx11, libxrender"
 TERMUX_PKG_BUILD_IN_SRC=true
@@ -65,6 +65,7 @@ termux_step_configure() {
 		--disable-precompiled-headers \
 		--disable-warnings-as-errors \
 		--enable-option-checking=fatal \
+		--enable-headless-only=yes \
 		--with-toolchain-type=gcc \
 		--with-jvm-variants=server \
 		--with-devkit="$TERMUX_STANDALONE_TOOLCHAIN" \
@@ -75,7 +76,6 @@ termux_step_configure() {
 		--with-freetype-lib="$TERMUX_PREFIX/lib" \
 		--with-libpng=system \
 		--with-zlib=system \
-		--with-x \
 		--x-includes="$TERMUX_PREFIX/include/X11" \
 		--x-libraries="$TERMUX_PREFIX/lib"
 }
--- packages/proot-distro/build.sh
+++ packages/proot-distro/build.sh
@@ -2,26 +2,30 @@ TERMUX_PKG_HOMEPAGE=https://github.com/termux/proot-distro
 TERMUX_PKG_DESCRIPTION="Termux official utility for managing proot'ed Linux distributions"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
-TERMUX_PKG_VERSION=1.9.0
+TERMUX_PKG_VERSION=2.1.1
 TERMUX_PKG_SRCURL=https://github.com/termux/proot-distro/archive/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=a4baaf11cc1325a68f6dc15610ef864c8f227b9ccd68a1e3571e1100df6a6022
+TERMUX_PKG_SHA256=5c80c9e5f332f758234a73d5055aaf817c43bc4363f2265ef185a5e43166bb08
 TERMUX_PKG_DEPENDS="bash, bzip2, coreutils, curl, findutils, gzip, ncurses-utils, proot (>= 5.1.107-32), sed, tar, xz-utils"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 
-# Allow to edit distribution plug-ins.
-TERMUX_PKG_CONFFILES="
-etc/proot-distro/alpine.sh
-etc/proot-distro/archlinux.sh
-etc/proot-distro/debian-buster.sh
-etc/proot-distro/fedora-33.sh
-etc/proot-distro/nethunter.sh
-etc/proot-distro/parrot-lts.sh
-etc/proot-distro/ubuntu-18.04.sh
-etc/proot-distro/ubuntu-20.04.sh
-etc/proot-distro/void.sh
-"
-
 termux_step_make_install() {
 	TERMUX_PREFIX="$TERMUX_PREFIX" TERMUX_ANDROID_HOME="$TERMUX_ANDROID_HOME" ./install.sh
+}
+
+termux_step_create_debscripts() {
+	cat <<- EOF > ./preinst
+	#!${TERMUX_PREFIX}/bin/bash
+	set -e
+	shopt -s nullglob
+
+	for i in ${TERMUX_PREFIX}/etc/proot-distro/*.sh; do
+	  if ! grep -qP "^\s*TARBALL_URL" "\$i"; then
+	    echo "Disabling old style v1.x proot-distro plug-in: \$(basename "\$i")"
+	    mv -f "\${i}" "\${i}.bak"
+	  fi
+	done
+
+	exit 0
+	EOF
 }
